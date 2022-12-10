ba1af0bac9a4b8bf3755f992c945dd6a9e9421d2
AnisurRahmanFaysal <faysal.swe27@gmail.com>
2022-12-10 17:23:26 +0600

    Eloquent delete operation

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -15,8 +15,15 @@ class HomeController extends Controller
     public function show(Request $request)
     {
         // $readOperation= Product::all();
-        $readOperation = Product::select('name','quantity','price')->where('id','=' ,1)->first();
+        // $readOperation = Product::select('name','quantity','price')->where('id','=' ,1)->first();
+        // Product::where('id','=',1)->update([
+        //     'name'=>'table',
+        //     'quantity'=>'500'
+        // ]);
         //dd($readOperation);
-        return $readOperation;
+        // return $readOperation;
+
+        Product::where('id','=',1)->delete();
+        return 'Successfully Updated!';
     }
 }
