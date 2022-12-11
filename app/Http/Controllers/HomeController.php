861c062f4a3e1c2948bcb57d9a929ab06f3410c8
AnisurRahmanFaysal <faysal.swe27@gmail.com>
2022-12-11 21:43:26 +0600

    Insert and select row quary

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,13 +23,24 @@ class HomeController extends Controller
 
     public function insertOperation()
     {
-        DB::table(table: 'products')->insert([
-            'id' => 1,
-            'name' => 'table',
-            'quantity' => 100,
-            'price' => 500
-        ]);
+        // DB::table(table: 'products')->insert([
+        //     'id' => 1,
+        //     'name' => 'table',
+        //     'quantity' => 100,
+        //     'price' => 500
+        // ]);
 
-        return 'Insert Successfully';
+        DB::insert('Insert into products(id,name,quantity,price) values(?,?,?,?)', [6, 'table', 250, 400]);
+
+        return 'Insertion Complete!';
+    }
+
+    public function readOperation()
+    {
+
+        //DB::table('products')->select('name', 'price', 'quantity')->where('id','=',6)->get();
+        //$product = DB::table('products')->select('name', 'price', 'quantity')->where('id', '=', 6)->get();
+        $product = DB::select('select name, price, quantity from products where id = 6');
+        return $product;
     }
 }
